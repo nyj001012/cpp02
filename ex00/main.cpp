3b457673b133d6dd4e874b648b2295685826f4ae
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-13 15:25:14 +0900

    feat: add initial files of ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/13 15:19:43 by yena              #+#    #+#             */
+/*   Updated: 2023/06/13 15:22:45 by yena             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(void) {
+  Fixed a;
+  Fixed b(a);
+  Fixed c;
+
+  c = b;
+
+  std::cout << a.getRawBits() << std::endl;
+  std::cout << b.getRawBits() << std::endl;
+  std::cout << c.getRawBits() << std::endl;
+
+  return 0;
+}
