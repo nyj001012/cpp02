de6d4d1c3bf24b6a357334d90263d38fd511e072
yena <50291995+nyj001012@users.noreply.github.com>
2023-06-17 17:31:13 +0900

    feat: implement Point class

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,12 +6,39 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 17:19:48 by yena              #+#    #+#             */
-/*   Updated: 2023/06/17 17:19:48 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/17 17:30:29 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//
-// Created by 나예진 on 2023/06/17.
-//
-
 #include "Point.hpp"
+
+Point::Point(void) {
+  this->_x = Fixed(0.0);
+  this->_y = Fixed(0.0);
+}
+
+Point::Point(const float x, const float y) {
+  this->_x = Fixed(x);
+  this->_y = Fixed(y);
+}
+
+Point::Point(const Point &object) {
+  *this = object;
+}
+
+Point &Point::operator=(const Point &ref) {
+  this->_x = ref.getX();
+  this->_y = ref.getY();
+  return *this;
+}
+
+Point::~Point(void) {
+}
+
+Fixed const &Point::getX(void) const {
+  return this->_x;
+}
+
+Fixed const &Point::getY(void) const {
+  return this->_y;
+}
--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,19 +6,31 @@
 /*   By: yena <yena@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/17 17:19:48 by yena              #+#    #+#             */
-/*   Updated: 2023/06/17 17:19:50 by yena             ###   ########.fr       */
+/*   Updated: 2023/06/17 17:28:30 by yena             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-//
-// Created by 나예진 on 2023/06/17.
-//
 
 #ifndef CPP02_EX03_POINT_HPP_
 #define CPP02_EX03_POINT_HPP_
 
-class Point {
+# include "Fixed.hpp"
 
+/**
+ * @brief Point class
+ */
+class Point {
+ private:
+  Fixed const _x;
+  Fixed const _y;
+
+ public:
+  Point(void);
+  Point(const float x, const float y);
+  Point(const Point &object);
+  Point &operator=(const Point &ref);
+  ~Point(void);
+  Fixed const &getX(void) const;
+  Fixed const &getY(void) const;
 };
 
 #endif //CPP02_EX03_POINT_HPP_
